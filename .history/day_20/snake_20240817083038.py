67f52c8da2a629349a1cc316bb072785aa27b492
Anamika Tiwari <155213271+Anamikatiwari@users.noreply.github.com>
2024-08-17 09:02:01 +0545

    day-20

---
+++ .history/day_20/snake_20240817083038.py
@@ -0,0 +1,41 @@
+# from turtle import Screen
+# from snake import Snake 
+# import time
+
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.bgcolor("black")
+# screen.title("My Snake game")
+# screen.tracer(0)
+
+# snake = Snake()
+
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+    
+#     snake.move()
+    
+    
+    
+    
+# screen.exitonclick()
+
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
--- .history/day_20/snake_game_20240817084015.py
+++ .history/day_20/snake_game_20240817084015.py
@@ -1,6 +1,7 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from snake import Snake
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,20 +10,17 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-# food = Food()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey
 
 game_is_on = True
-while game_is_on:
+while  game_is_on:
     screen.update()
     time.sleep(0.1)
-
+    
     snake.move()
 
-
+        
 screen.exitonclick()
+
